ee508a891fdcf2936de77b754129c84a4ee5826d
Roope Kuisma <roope.kuisma@gmail.com>
2016-05-28 21:43:32 +0300

    Initial changes
    Removed AVR hardware related crap and added application.h

--- Adafruit_GFX.cpp
+++ Adafruit_GFX.cpp
@@ -31,11 +31,6 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 */
 
-#ifdef __AVR__
- #include <avr/pgmspace.h>
-#elif defined(ESP8266)
- #include <pgmspace.h>
-#endif
 #include "Adafruit_GFX.h"
 #include "glcdfont.c"
 
@@ -466,11 +461,7 @@ void Adafruit_GFX::drawXBitmap(int16_t x, int16_t y,
   }
 }
 
-#if ARDUINO >= 100
 size_t Adafruit_GFX::write(uint8_t c) {
-#else
-void Adafruit_GFX::write(uint8_t c) {
-#endif
 
   if(!gfxFont) { // 'Classic' built-in font
 
@@ -516,9 +507,7 @@ void Adafruit_GFX::write(uint8_t c) {
     }
 
   }
-#if ARDUINO >= 100
   return 1;
-#endif
 }
 
 // Draw a character
